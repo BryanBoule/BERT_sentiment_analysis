5b14b4c7a89076b25bb3fd1a5e59581a1d5cec5e
Bryan <bboule@solocal.com>
2021-01-13 11:51:37 +0100

    init test

---
+++ test_script.py
@@ -0,0 +1,9 @@
+import numpy as np
+import pandas as pd
+
+if __name__ == '__main__':
+    print('Hello world')
+    k = np.array([1,2,3])
+    print(k)
+    data = pd.DataFrame({'col1': [1,2,3], 'col2': [1,2,3]})
+    print(data.head())
